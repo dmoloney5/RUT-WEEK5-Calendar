60d63de679b8725fb94fafbc0812db685962bbeb
techman <dmoloney5@gmail.com>
2022-02-02 15:09:34 -0500

    fixed mistake in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,16 +42,16 @@ $(document).ready(function () {
     }
 
     // Get item from local storage if any
-    $("#hour* .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour8.description").val(localStorage.getItem("hour8"));
+    $("#hour9.description").val(localStorage.getItem("hour9"));
+    $("#hour10.description").val(localStorage.getItem("hour10"));
+    $("#hour11.description").val(localStorage.getItem("hour11"));
+    $("#hour12.description").val(localStorage.getItem("hour12"));
+    $("#hour1.description").val(localStorage.getItem("hour1"));
+    $("#hour2.description").val(localStorage.getItem("hour2"));
+    $("#hour3.description").val(localStorage.getItem("hour3"));
+    $("#hour4.description").val(localStorage.getItem("hour4"));
+    $("#hour5.description").val(localStorage.getItem("hour5"));
 
     timeTracker();
 })
